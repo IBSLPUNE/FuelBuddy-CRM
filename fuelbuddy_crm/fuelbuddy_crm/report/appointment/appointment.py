65badd97b4ebec984feca225d7e90d64260992b7
Lucky <lucky@indibasolutions.com>
2024-12-09 14:57:08 +0530

    updated

--- fuelbuddy_crm/fuelbuddy_crm/report/appointment/appointment.py
+++ fuelbuddy_crm/fuelbuddy_crm/report/appointment/appointment.py
@@ -8,155 +8,154 @@ def execute(filters=None):
 		business_head,
 		posting_date,
 		name,
-		sales_person,
 		ROW_NUMBER() OVER (PARTITION BY business_head ORDER BY posting_date DESC) AS rank
-	    FROM 
-		`tabSales Target`
+	    FROM `tabSales Target`
 	),
-	sales_person AS (
-	    SELECT name AS team_user_name,
-		   custom_user, custom_team
+	filtered_sales_person AS (
+	    SELECT 
+		name AS team_user_name,
+		custom_user, 
+		custom_team
 	    FROM `tabSales Person`
-		WHERE name NOT IN ('Abhishek Anand','Ankur Goel','Narayan Sathe','Nora Bali','Palash Gupta','Ravi','SK Arora','Setu Joshi','Vadivel Nandhyalam')
+	    WHERE name NOT IN (
+		'Abhishek Anand', 'Ankur Goel', 'Narayan Sathe', 
+		'Nora Bali', 'Palash Gupta', 'Ravi', 
+		'SK Arora', 'Setu Joshi', 'Vadivel Nandhyalam'
+	    )
 	),
-	cte AS (
-	    SELECT DISTINCT 
-		td.reference_type,
-		td.reference_name,
+	todo_data AS (
+	    SELECT 
 		td.owner,
 		td.name AS Todo_ID,
+		td.reference_type,
+		td.reference_name,
 		CASE 
-		    WHEN date = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) AND td.status != 'Cancelled' THEN 
+		    WHEN td.date = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) THEN 
 		        CASE 
-		            WHEN td.reference_name = tc.name THEN tc.customer_name 
-		            WHEN td.reference_name = tl.name THEN tl.company_name 
-		            WHEN td.reference_name = tpo.name THEN tpo.customer_name 
+		            WHEN td.reference_name = tc.name THEN tc.customer_name
+		            WHEN td.reference_name = tl.name THEN tl.company_name
+		            WHEN td.reference_name = tpo.name THEN tpo.customer_name
 		            WHEN td.reference_name = ep.parent THEN 
 		                CASE 
-		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name 
-		                    WHEN ep.reference_docname = tl.name THEN tl.company_name 
-		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name 
-		                    ELSE ep.reference_docname 
+		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name
+		                    WHEN ep.reference_docname = tl.name THEN tl.company_name
+		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name
+		                    ELSE ep.reference_docname
 		                END
-		            ELSE td.reference_name 
+		            ELSE td.reference_name
 		        END
 		    ELSE '-' 
 		END AS Yesterday,
 		CASE 
-		    WHEN date = CURRENT_DATE() AND td.status != 'Cancelled' THEN 
+		    WHEN td.date = CURRENT_DATE() THEN 
 		        CASE 
-		            WHEN td.reference_name = tc.name THEN tc.customer_name 
-		            WHEN td.reference_name = tl.name THEN tl.company_name 
-		            WHEN td.reference_name = tpo.name THEN tpo.customer_name 
+		            WHEN td.reference_name = tc.name THEN tc.customer_name
+		            WHEN td.reference_name = tl.name THEN tl.company_name
+		            WHEN td.reference_name = tpo.name THEN tpo.customer_name
 		            WHEN td.reference_name = ep.parent THEN 
 		                CASE 
-		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name 
+		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name
 		                    WHEN ep.reference_docname = tl.name THEN tl.company_name
-		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name 
-		                    ELSE ep.reference_docname 
+		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name
+		                    ELSE ep.reference_docname
 		                END
-		            ELSE td.reference_name 
-		        END 
+		            ELSE td.reference_name
+		        END
 		    ELSE '-' 
 		END AS Today,
 		CASE 
-		    WHEN date = DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY) AND td.status != 'Cancelled' THEN 
+		    WHEN td.date = DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY) THEN 
 		        CASE 
-		            WHEN td.reference_name = tc.name THEN tc.customer_name 
-		            WHEN td.reference_name = tl.name THEN tl.company_name 
-		            WHEN td.reference_name = tpo.name THEN tpo.customer_name 
+		            WHEN td.reference_name = tc.name THEN tc.customer_name
+		            WHEN td.reference_name = tl.name THEN tl.company_name
+		            WHEN td.reference_name = tpo.name THEN tpo.customer_name
 		            WHEN td.reference_name = ep.parent THEN 
 		                CASE 
-		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name 
-		                    WHEN ep.reference_docname = tl.name THEN tl.company_name 
-		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name 
-		                    ELSE ep.reference_docname 
+		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name
+		                    WHEN ep.reference_docname = tl.name THEN tl.company_name
+		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name
+		                    ELSE ep.reference_docname
 		                END
-		            ELSE td.reference_name 
-		        END 
+		            ELSE td.reference_name
+		        END
 		    ELSE '-' 
 		END AS Tomorrow,
 		CASE 
 		    WHEN EXTRACT(WEEK FROM td.date) = EXTRACT(WEEK FROM CURRENT_DATE()) 
-		        AND EXTRACT(YEAR FROM td.date) = EXTRACT(YEAR FROM CURRENT_DATE()) 
-		        AND td.status != 'Cancelled' THEN 
+		      AND EXTRACT(YEAR FROM td.date) = EXTRACT(YEAR FROM CURRENT_DATE()) THEN 
 		        CASE 
-		            WHEN td.reference_name = tc.name THEN tc.customer_name 
-		            WHEN td.reference_name = tl.name THEN tl.company_name 
-		            WHEN td.reference_name = tpo.name THEN tpo.customer_name 
+		            WHEN td.reference_name = tc.name THEN tc.customer_name
+		            WHEN td.reference_name = tl.name THEN tl.company_name
+		            WHEN td.reference_name = tpo.name THEN tpo.customer_name
 		            WHEN td.reference_name = ep.parent THEN 
 		                CASE 
-		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name 
-		                    WHEN ep.reference_docname = tl.name THEN tl.company_name 
-		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name 
-		                    ELSE ep.reference_docname 
+		                    WHEN ep.reference_docname = tc.name THEN tc.customer_name
+		                    WHEN ep.reference_docname = tl.name THEN tl.company_name
+		                    WHEN ep.reference_docname = tpo.name THEN tpo.customer_name
+		                    ELSE ep.reference_docname
 		                END
-		            ELSE td.reference_name 
-		        END 
+		            ELSE td.reference_name
+		        END
 		    ELSE '-' 
-		END AS Week,
-		COUNT(CASE WHEN td.status = 'Open' THEN td.name ELSE NULL END) AS Count_Status_Open
+		END AS Week
 	    FROM 
 		`tabToDo` td
-	    LEFT JOIN 
-		`tabEvent Participants` ep 
-		ON ep.parent = td.reference_name AND ep.idx = 1
-	    LEFT JOIN 
-		`tabCustomer` tc 
-		ON tc.name = td.reference_name OR ep.reference_docname = tc.name
-	    LEFT JOIN 
-		`tabLead` tl 
-		ON tl.name = td.reference_name OR ep.reference_docname = tl.name
-	    LEFT JOIN 
-		`tabOpportunity` tpo 
-		ON tpo.name = td.reference_name OR ep.reference_docname = tpo.name
-	    WHERE td.status = 'Open' AND td.reference_type IN ('Customer', 'Lead', 'Opportunity','Event') 
-	    AND (
-		DATE(td.date) = CURDATE()
-		OR DATE(td.date) = CURDATE() - INTERVAL 1 DAY
-		OR WEEK(DATE(td.date)) = WEEK(CURDATE())
-	    )
-	    GROUP BY 
-		1, 2, 3, 4, 5, 6, 7,8
-	), 
-	final_cte as (
-	SELECT 
-	    Todo_ID,
-	    reference_name,
-	    reference_type,
-	    owner,
-	    Yesterday,
-	    Today,
-	    Tomorrow,
-	    Week,
-	    Count_Status_Open
-	FROM cte 
-	WHERE 
-	    (Yesterday != '-' OR 
-	    Today != '-' OR 
-	    Tomorrow != '-' OR 
-	    Week != '-')
+	    LEFT JOIN `tabEvent Participants` ep ON ep.parent = td.reference_name AND ep.idx = 1
+	    LEFT JOIN `tabCustomer` tc ON tc.name = td.reference_name OR ep.reference_docname = tc.name
+	    LEFT JOIN `tabLead` tl ON tl.name = td.reference_name OR ep.reference_docname = tl.name
+	    LEFT JOIN `tabOpportunity` tpo ON tpo.name = td.reference_name OR ep.reference_docname = tpo.name
+	    WHERE 
+		td.status = 'Open'
+		AND td.date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
+	),
+	todo_status_count AS (
+	    SELECT 
+		td.owner,
+		COUNT(*) AS Count_Status_Open
+	    FROM `tabToDo` td
+	    WHERE 
+		td.status = 'Open'
+		AND td.date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
+	    GROUP BY td.owner
+	),
+	final_todo_data AS (
+	    SELECT 
+		td.Todo_ID,
+		td.reference_name,
+		td.reference_type,
+		td.owner,
+		td.Yesterday,
+		td.Today,
+		td.Tomorrow,
+		td.Week,
+		COALESCE(tsc.Count_Status_Open, 0) AS Count_Status_Open
+	    FROM todo_data td
+	    LEFT JOIN todo_status_count tsc ON tsc.owner = td.owner
+	    WHERE 
+		td.Yesterday != '-' OR td.Today != '-' OR td.Tomorrow != '-' OR td.Week != '-'
 	)
-	select distinct
-	sp.custom_team AS Team,
-	sp.team_user_name AS BD,
-	bd.zonal_head_sales_person AS Zonal_head,
-	case when reference_type = 'Event' then reference_name else Todo_ID end as Todo_ID,
-	case when reference_type = 'Event' then 'Event' else 'Task' end as Appointment_type,
-	reference_type AS Reference_Type,
-	todo.Yesterday,
-	todo.Today,
-	todo.Week,
-	todo.Count_Status_Open
-	from ranked_data st
-	left join `tabBusiness Deverloper` bd on bd.parent = st.name
-	LEFT JOIN sales_person sp ON sp.custom_user = bd.bd
-	left join final_cte todo on todo.owner = bd.bd
-	where rank = 1
-	and (Yesterday != '-' OR 
-	    Today != '-' OR 
-	    Tomorrow != '-' OR 
-	    Week != '-')
+
+	SELECT DISTINCT
+	    sp.custom_team AS Team,
+	    sp.team_user_name AS BD,
+	    bd.zonal_head_sales_person AS Zonal_head,
+	    todo.Todo_ID,
+	    CASE 
+		WHEN todo.reference_type = 'Event' THEN 'Event' 
+		ELSE 'Task' 
+	    END AS Appointment_type,
+	    todo.reference_type AS Reference_Type,
+	    todo.Yesterday,
+	    todo.Today,
+	    todo.Tomorrow,
+	    todo.Week,
+	    COALESCE(co.Count_Status_Open, 0) AS Count_Status_Open
+	FROM ranked_data st
+	LEFT JOIN `tabBusiness Deverloper` bd ON bd.parent = st.name
+	LEFT JOIN filtered_sales_person sp ON sp.custom_user = bd.bd
+	LEFT JOIN final_todo_data todo ON LOWER(todo.owner) = LOWER(bd.bd)
+	LEFT JOIN todo_status_count co ON bd.bd= co.owner
+	WHERE st.rank = 1
       AND ((bd.zonal_head = %s) OR (bd.bd = %s) OR (st.business_head = %s))
       ORDER BY Team, BD
     """
